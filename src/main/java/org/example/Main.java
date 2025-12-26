eabe6413f26075c0fc9059f25ddfb597a4297627
hursha <hurshaparveen@gmail.com>
2025-12-25 19:11:11 -0600

    updated main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
+        ConditionalExercises conditionalExercises= new ConditionalExercises();
+        System.out.println(conditionalExercises.lessThanFive(2));
+        System.out.println(conditionalExercises.getAgeGroup(30));
+        System.out.println(conditionalExercises.isValidPassword("mottu0317"));
+
+        LoopExercises loopExercises = new LoopExercises();
+        System.out.println(loopExercises.sum(5));
+        System.out.println(loopExercises.sumUntilEven(10));
+
         JavaCafe javaCafe = new JavaCafe();
         javaCafe.run();
+
     }
 }
